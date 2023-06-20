3aa4af37d3734327c93cdd02008412bfeda8a93d
Carlos Quintero <cquintero@urudata.com>
2023-06-20 13:33:10 -0300

    Mejoras en el nosotros

--- app.js
+++ app.js
@@ -54,50 +54,50 @@ var swiper = new Swiper(".swiper", {
   closeEnX.addEventListener('click', ()=> {
 	containerImageExpan.classList.toggle('move');
 	imageShow.classList.toggle('show');
-  })
-
-  // Mapa del footer
-  function initMap() {
-	var location = { lat: 40.7128, lng: -74.0060 }; // Coordenadas de ubicación (ejemplo: Nueva York)
-  
-	var map = new google.maps.Map(document.getElementById('map'), {
-	  center: location,
-	  zoom: 12
-	});
-  
-	var marker = new google.maps.Marker({
-	  position: location,
-	  map: map,
-	  title: 'Ubicación'
-	});
-  }
-
-  // Formulario
-
-  document.getElementById("miFormulario").addEventListener("submit", function(event) {
-    event.preventDefault(); // Evita que el formulario se envíe de forma predeterminada
-
-    // Obtiene los datos del formulario
-    var formData = new FormData(document.getElementById("miFormulario"));
-
-    // Realiza la solicitud de envío utilizando Fetch
-    fetch("procesar_formulario.php", {
-      method: "POST",
-      body: formData
-    })
-    .then(function(response) {
-      // Maneja la respuesta de la solicitud
-      if (response.ok) {
-        // La solicitud se completó correctamente
-        console.log("Formulario enviado correctamente");
-      } else {
-        // La solicitud no se completó correctamente
-        console.log("Error al enviar el formulario");
-      }
-    })
-    .catch(function(error) {
-      // Maneja cualquier error de la solicitud
-      console.log("Error en la solicitud: " + error);
-    });
   });
+
+//   // Mapa del footer
+//   function initMap() {
+// 	var location = { lat: 40.7128, lng: -74.0060 }; // Coordenadas de ubicación (ejemplo: Nueva York)
+  
+// 	var map = new google.maps.Map(document.getElementById('map'), {
+// 	  center: location,
+// 	  zoom: 12
+// 	});
+  
+// 	var marker = new google.maps.Marker({
+// 	  position: location,
+// 	  map: map,
+// 	  title: 'Ubicación'
+// 	});
+//   }
+
+//   // Formulario
+
+//   document.getElementById("miFormulario").addEventListener("submit", function(event) {
+//     event.preventDefault(); // Evita que el formulario se envíe de forma predeterminada
+
+//     // Obtiene los datos del formulario
+//     var formData = new FormData(document.getElementById("miFormulario"));
+
+//     // Realiza la solicitud de envío utilizando Fetch
+//     fetch("procesar_formulario.php", {
+//       method: "POST",
+//       body: formData
+//     })
+//     .then(function(response) {
+//       // Maneja la respuesta de la solicitud
+//       if (response.ok) {
+//         // La solicitud se completó correctamente
+//         console.log("Formulario enviado correctamente");
+//       } else {
+//         // La solicitud no se completó correctamente
+//         console.log("Error al enviar el formulario");
+//       }
+//     })
+//     .catch(function(error) {
+//       // Maneja cualquier error de la solicitud
+//       console.log("Error en la solicitud: " + error);
+//     });
+//   });
 
